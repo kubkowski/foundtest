54cc570ed9fcab1f3eeabbf3e4e8ab08e1985488
kubkowski <jakub.opara@gmail.com>
2014-03-28 14:09:22 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundtest::Application.config.secret_key_base = '33097a56f862a23dfe93d5b478ee1c23d7ef54067886e0c9d5e26f6f80b8c824928a622e2c1171e544b4222dcdeee753151766caa7ffba75ff032f3ef3f457f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foundtest::Application.config.session_store :cookie_store, key: '_foundtest_session'
